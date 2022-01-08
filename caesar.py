34c220fe56242dccf994d461f470308ec0ab4ed0
ChaoqunCheng <chengchaoqun@hotmail.com>
2022-01-08 23:00:21 +0800

    finish 582 hw1

--- caesar.py
+++ caesar.py
@@ -1,29 +1,15 @@
-
 def encrypt(key,plaintext):
     ciphertext = ""
     key %= 26
     #YOUR CODE HERE
     for char in plaintext:
-        if char != " ":
+        if char.isalpha():
             code = (ord(char.upper()) - 65 + key) % 26
             ciphertext += chr((code+65))
         else:
-            ciphertext += " "
+            ciphertext += char
     return ciphertext
 
 def decrypt(key,ciphertext):
-    plaintext=""
-    key %= 26
-    #YOUR CODE HERE
-    for char in ciphertext:
-        if char != " ":
-            code = (ord(char.upper()) - 65 - key + 26) % 26
-            plaintext += chr((code+65))
-        else:
-            plaintext += " "
+    plaintext = encrypt(-key, ciphertext)
     return plaintext
-
-
-if __name__ == '__main__':
-    print(encrypt(1, "BASE"))
-    print(decrypt(1, encrypt(1, "BASE")))
